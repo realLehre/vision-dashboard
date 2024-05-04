60592d9aa43c898b7c98c47ff1294ec69b339e0f
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-04 06:11:59 +0100

    refactor: side bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,7 @@ export const routes: Routes = [
   { path: 'tables', component: AppComponent },
   { path: 'billing', component: AppComponent },
   { path: 'rtl', component: AppComponent },
+  { path: 'profile', component: AppComponent },
+  { path: 'sign-in', component: AppComponent },
+  { path: 'sign-up', component: AppComponent },
 ];
