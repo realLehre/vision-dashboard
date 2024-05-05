14341c33d3f5c38acedeac99677cebe82f433e28
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-05 15:51:31 +0100

    feat: records section

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { QuickInfoComponent } from './quick-info/quick-info.component';
+import { RecordsComponent } from './records/records.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [QuickInfoComponent],
+  imports: [QuickInfoComponent, RecordsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
