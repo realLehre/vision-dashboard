f44a75d50e896a5f2582a31d59960978bac5120f
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-07 20:20:47 +0100

    feat: made likes progress dynamic

--- src/app/features/dashboard/records/tracks/tracks.component.ts
+++ src/app/features/dashboard/records/tracks/tracks.component.ts
@@ -8,19 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './tracks.component.scss',
 })
 export class TracksComponent {
-  progress: number = 0;
-
-  generatePath(progress: number): string {
-    const radius = 50;
-    const centerX = 50;
-    const centerY = 50;
-    const startAngle = -Math.PI / 2; // Start at the top center
-    const endAngle = startAngle + Math.PI * (progress / 50); // Convert progress to radians
-    const x1 = centerX + radius * Math.cos(startAngle);
-    const y1 = centerY + radius * Math.sin(startAngle);
-    const x2 = centerX + radius * Math.cos(endAngle);
-    const y2 = centerY + radius * Math.sin(endAngle);
-    const largeArcFlag = progress > 50 ? 1 : 0; // 1 if progress > 50, else 0
-    return `M ${x1},${y1} A ${radius},${radius} 0 ${largeArcFlag},1 ${x2},${y2}`;
-  }
+  progress: number = 70;
 }
