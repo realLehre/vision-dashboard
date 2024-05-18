d9872e78b37fe1bc944b29a6be3bb0e44295b793
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-19 00:11:01 +0100

    feat: close side menu button

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDrawerContainer } from '@angular/material/sidenav';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { LayoutService } from './services/layout.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [MatSidenavModule, SidenavComponent, MainContentComponent],
+  imports: [
+    MatSidenavModule,
+    SidenavComponent,
+    MainContentComponent,
+    MatTooltipModule,
+    NgClass,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  isMenuOpened: boolean =
+    JSON.parse(sessionStorage.getItem('menuStatus')!) ?? true;
+  onToggleMenu() {
+    this.isMenuOpened = !this.isMenuOpened;
+    sessionStorage.setItem('menuStatus', JSON.stringify(this.isMenuOpened));
+  }
+}
