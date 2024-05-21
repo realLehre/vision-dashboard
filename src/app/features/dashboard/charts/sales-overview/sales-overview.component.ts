95c5c9559721e302a710d5d40de2592bb8b93ff1
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-21 16:49:57 +0100

    feat: congif chart

--- src/app/features/dashboard/charts/sales-overview/sales-overview.component.ts
+++ src/app/features/dashboard/charts/sales-overview/sales-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -26,23 +26,31 @@ export type ChartOptions = {
   templateUrl: './sales-overview.component.html',
   styleUrl: './sales-overview.component.scss',
 })
-export class SalesOverviewComponent {
+export class SalesOverviewComponent implements OnInit {
   // @ViewChild('chart') chart: ChartComponent;
-  public chartOptions: Partial<ChartOptions | any>;
+  chartOptions!: Partial<ChartOptions | any>;
+  currentYear = new Date().getFullYear();
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
+    this.setChart();
+  }
+
+  setChart() {
     this.chartOptions = {
       series: [
         {
           name: 'series1',
-          data: [31, 40, 28, 51, 42, 109, 100],
+          data: [31, 40, 28, 51, 42, 109, 200, 40, 28, 51, 42, 109, 200],
         },
         {
           name: 'series2',
-          data: [11, 32, 45, 32, 34, 52, 41],
+          data: [11, 32, 45, 32, 34, 52, 41, 40, 28, 51, 42, 109, 200],
         },
       ],
       chart: {
+        width: '100%',
         height: 350,
         type: 'area',
       },
@@ -53,20 +61,60 @@ export class SalesOverviewComponent {
         curve: 'smooth',
       },
       xaxis: {
-        type: 'datetime',
+        type: 'month',
         categories: [
-          '2018-09-19T00:00:00.000Z',
-          '2018-09-19T01:30:00.000Z',
-          '2018-09-19T02:30:00.000Z',
-          '2018-09-19T03:30:00.000Z',
-          '2018-09-19T04:30:00.000Z',
-          '2018-09-19T05:30:00.000Z',
-          '2018-09-19T06:30:00.000Z',
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
         ],
+        labels: {
+          style: {
+            colors: '#fff',
+            fontSize: '10px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+          },
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: true,
+          style: {
+            colors: '#fff',
+            fontSize: '10px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+          },
+        },
       },
       tooltip: {
         x: {
           format: 'dd/MM/yy HH:mm',
+        },
+      },
+      grid: {
+        show: true,
+        borderColor: '#56577A',
+        strokeDashArray: 4,
+        xaxis: {
+          lines: {
+            show: false,
+          },
         },
       },
     };
