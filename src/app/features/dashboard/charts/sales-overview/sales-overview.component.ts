6e5185bf6df122efef7fa1ebfb27481a1c56cff0
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-21 18:02:00 +0100

    feat: sales overview chart

--- src/app/features/dashboard/charts/sales-overview/sales-overview.component.ts
+++ src/app/features/dashboard/charts/sales-overview/sales-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import {
   ChartComponent,
   ApexAxisChartSeries,
@@ -9,6 +9,7 @@ import {
   ApexStroke,
   NgApexchartsModule,
 } from 'ng-apexcharts';
+import { SalesOverviewService } from './services/sales-overview.service';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
@@ -27,9 +28,9 @@ export type ChartOptions = {
   styleUrl: './sales-overview.component.scss',
 })
 export class SalesOverviewComponent implements OnInit {
-  // @ViewChild('chart') chart: ChartComponent;
   chartOptions!: Partial<ChartOptions | any>;
   currentYear = new Date().getFullYear();
+  salesOverviewService = inject(SalesOverviewService);
 
   constructor() {}
 
@@ -38,101 +39,7 @@ export class SalesOverviewComponent implements OnInit {
   }
 
   setChart() {
-    this.chartOptions = {
-      series: [
-        {
-          name: 'series1',
-          data: [31, 40, 28, 51, 42, 109, 200, 40, 28, 51, 42, 109, 200],
-        },
-        {
-          name: 'series2',
-          data: [11, 32, 45, 32, 34, 52, 41, 40, 28, 51, 42, 109, 200],
-        },
-      ],
-      chart: {
-        width: '100%',
-        height: 350,
-        type: 'area',
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      xaxis: {
-        type: 'month',
-        categories: [
-          'Jan',
-          'Feb',
-          'Mar',
-          'Apr',
-          'May',
-          'Jun',
-          'Jul',
-          'Aug',
-          'Sep',
-          'Oct',
-          'Nov',
-          'Dec',
-        ],
-        labels: {
-          style: {
-            colors: '#fff',
-            fontSize: '10px',
-            fontFamily: 'Helvetica, Arial, sans-serif',
-            fontWeight: 400,
-          },
-        },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-      },
-      yaxis: {
-        labels: {
-          show: true,
-          style: {
-            colors: '#fff',
-            fontSize: '10px',
-            fontFamily: 'Helvetica, Arial, sans-serif',
-            fontWeight: 400,
-          },
-        },
-      },
-      tooltip: {
-        x: {
-          format: 'dd/MM/yy HH:mm',
-        },
-      },
-      grid: {
-        show: true,
-        borderColor: '#56577A',
-        strokeDashArray: 4,
-        xaxis: {
-          lines: {
-            show: false,
-          },
-        },
-      },
-    };
-  }
-
-  public generateData(baseval: any, count: any, yrange: any) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
-      var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-      var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
-
-      series.push([x, y, z]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
+    this.salesOverviewService.getRandomHexColor();
+    this.chartOptions = this.salesOverviewService.setChart();
   }
 }
---
+++ src/app/features/dashboard/charts/sales-overview/services/sales-overview.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SalesOverviewService {
+  chartData: { name: string; data: number[] }[] = [
+    {
+      name: 'Product 1',
+      data: [400, 180, 300, 100, 410, 80, 288, 384, 222, 122, 100, 42],
+    },
+    {
+      name: 'Product 2',
+      data: [200, 380, 120, 230, 190, 330, 188, 254, 422, 302, 170, 420],
+    },
+  ];
+
+  categories: string[] = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  colors: string[] = ['#2CD9FF', '#0075FF'];
+  constructor() {}
+
+  setChart() {
+    return {
+      series: [...this.chartData],
+      legend: {
+        show: false,
+      },
+      chart: {
+        width: '100%',
+        height: 350,
+        type: 'area',
+        toolbar: { show: false },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+        colors: [...this.colors],
+      },
+      fill: {
+        colors: [...this.colors],
+        type: 'gradient',
+        gradient: {
+          type: 'vertical',
+          shadeIntensity: 1,
+          opacityFrom: 0.8,
+          gradientToColors: [...this.colors],
+          opacityTo: 0,
+          stops: [0, 100],
+        },
+      },
+      xaxis: {
+        type: 'month',
+        categories: [...this.categories],
+        labels: {
+          style: {
+            colors: '#fff',
+            fontSize: '10px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+          },
+        },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+      },
+      yaxis: {
+        labels: {
+          show: true,
+          style: {
+            colors: '#fff',
+            fontSize: '10px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 400,
+          },
+        },
+      },
+
+      tooltip: {
+        theme: 'dark',
+        x: {
+          format: 'dd/MM/yy HH:mm',
+        },
+      },
+      grid: {
+        show: true,
+        borderColor: '#56577A',
+        strokeDashArray: 4,
+        xaxis: {
+          lines: {
+            show: false,
+          },
+        },
+      },
+    };
+  }
+
+  getRandomHexColor() {
+    for (let i = 2; i < this.chartData.length; i++) {
+      const hex = Math.floor(Math.random() * 16777215).toString(16);
+      this.colors.push(`#${hex.padStart(6, '0').toUpperCase()}`);
+    }
+  }
+}
