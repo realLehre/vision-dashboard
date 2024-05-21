87196e2520962dee0fc2604d57b6041ae4f9b2e3
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-21 18:13:55 +0100

    feat: active users

--- src/app/features/dashboard/charts/charts.component.ts
+++ src/app/features/dashboard/charts/charts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SalesOverviewComponent } from './sales-overview/sales-overview.component';
+import { ActiveUsersComponent } from './active-users/active-users.component';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [SalesOverviewComponent],
+  imports: [SalesOverviewComponent, ActiveUsersComponent],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.scss',
 })
