2d04d2d27decefc424e23283731acf5f737ab81e
David Egbuwalo <lereegbuwalo@gmail.com>
2024-05-24 15:23:16 +0100

    refactor charts

--- src/app/features/dashboard/charts/charts.component.ts
+++ src/app/features/dashboard/charts/charts.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { SalesOverviewComponent } from './sales-overview/sales-overview.component';
 import { ActiveUsersComponent } from './active-users/active-users.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [SalesOverviewComponent, ActiveUsersComponent],
+  imports: [SalesOverviewComponent, ActiveUsersComponent, NgClass],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.scss',
 })
-export class ChartsComponent {}
+export class ChartsComponent implements OnInit {
+  @ViewChild('charts', { static: true })
+  chartsContainer!: ElementRef<HTMLDivElement>;
+  containerWidth!: number;
+
+  ngOnInit() {
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.containerWidth = this.chartsContainer.nativeElement.offsetWidth;
+  }
+}
