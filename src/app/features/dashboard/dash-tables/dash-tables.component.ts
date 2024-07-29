e4a429c90441318203b6343b99e0722dcc891bd0
David Egbuwalo <lereegbuwalo@gmail.com>
2024-07-29 19:36:08 +0100

    feat: orders overview

--- src/app/features/dashboard/dash-tables/dash-tables.component.ts
+++ src/app/features/dashboard/dash-tables/dash-tables.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProjectsComponent } from './projects/projects.component';
+import { OrdersOverviewComponent } from '../orders-overview/orders-overview.component';
 
 @Component({
   selector: 'app-dash-tables',
   standalone: true,
-  imports: [ProjectsComponent],
+  imports: [ProjectsComponent, OrdersOverviewComponent],
   templateUrl: './dash-tables.component.html',
   styleUrl: './dash-tables.component.scss',
 })
---
+++ src/app/features/dashboard/orders-overview/orders-overview.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders-overview.component.html',
+  styleUrl: './orders-overview.component.scss',
+})
+export class OrdersOverviewComponent {
+  ordersOverview: { order: string; date: string }[] = [
+    { order: '$2400 design changes', date: '22 DEC 7:20 PM' },
+    { order: 'NEW order', date: '23 DEC 7:00 PM' },
+    { order: 'Server Payments for April', date: '25 DEC 10:00 AM' },
+    { order: 'New card added for order', date: '25 DEC 7:20 AM' },
+    { order: 'Unlocked packages for Development', date: '22 DEC 7:20 PM' },
+    { order: 'New order', date: '22 DEC 7:20 PM' },
+  ];
+}
