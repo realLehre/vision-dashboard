fe499d2d1259283b5aba00b565ab58b3bd56a088
David Egbuwalo <lereegbuwalo@gmail.com>
2024-07-30 13:07:21 +0100

    feat: add responsiveness to dashboard tables

--- src/app/features/dashboard/dash-tables/dash-tables.component.ts
+++ src/app/features/dashboard/dash-tables/dash-tables.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, WritableSignal } from '@angular/core';
 import { ProjectsComponent } from './projects/projects.component';
 import { OrdersOverviewComponent } from '../orders-overview/orders-overview.component';
+import { ActiveUsersComponent } from '../charts/active-users/active-users.component';
+import { NgClass } from '@angular/common';
+import { DashboardService } from '../services/dashboard.service';
 
 @Component({
   selector: 'app-dash-tables',
   standalone: true,
-  imports: [ProjectsComponent, OrdersOverviewComponent],
+  imports: [
+    ProjectsComponent,
+    OrdersOverviewComponent,
+    ActiveUsersComponent,
+    NgClass,
+  ],
   templateUrl: './dash-tables.component.html',
   styleUrl: './dash-tables.component.scss',
 })
-export class DashTablesComponent {}
+export class DashTablesComponent {
+  dashboardService = inject(DashboardService);
+  containerWidth: WritableSignal<number> = this.dashboardService.dashboardWidth;
+}
--- src/app/features/dashboard/orders-overview/orders-overview.component.ts
+++ src/app/features/dashboard/orders-overview/orders-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders-overview',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './orders-overview.component.html',
   styleUrl: './orders-overview.component.scss',
 })
-export class OrdersOverviewComponent {
+export class OrdersOverviewComponent implements OnInit {
   ordersOverview: { order: string; date: string }[] = [
     { order: '$2400 design changes', date: '22 DEC 7:20 PM' },
     { order: 'NEW order', date: '23 DEC 7:00 PM' },
@@ -15,5 +15,11 @@ export class OrdersOverviewComponent {
     { order: 'New card added for order', date: '25 DEC 7:20 AM' },
     { order: 'Unlocked packages for Development', date: '22 DEC 7:20 PM' },
     { order: 'New order', date: '22 DEC 7:20 PM' },
+    { order: 'New order', date: '22 DEC 7:20 PM' },
+    // { order: 'New order', date: '22 DEC 7:20 PM' },
   ];
+
+  ngOnInit() {
+    // this.ordersOverview = [...this.ordersOverview, ...this.ordersOverview];
+  }
 }
