1fc53fb06f4b962d976f1475776e10d2457c98af
David Egbuwalo <lereegbuwalo@gmail.com>
2024-08-04 13:58:06 +0100

    refactor: some responsive fixes

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import { ProfileWelcomeComponent } from './profile-welcome/profile-welcome.component';
 import { ProfileCarInfoComponent } from './profile-car-info/profile-car-info.component';
 import { ProfileInfoComponent } from './profile-info/profile-info.component';
 import { ProfilePlatformSettingsComponent } from './profile-platform-settings/profile-platform-settings.component';
 import { ProfileProjectsComponent } from './profile-projects/profile-projects.component';
+import { LayoutService } from '../../core/layout/services/layout.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -16,8 +18,11 @@ import { ProfileProjectsComponent } from './profile-projects/profile-projects.co
     ProfileInfoComponent,
     ProfilePlatformSettingsComponent,
     ProfileProjectsComponent,
+    NgClass,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  layoutService = inject(LayoutService);
+}
