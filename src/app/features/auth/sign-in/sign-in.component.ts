f2b9c2a80552fe9f72af3ca5c8688a57dbcd45cf
David Egbuwalo <lereegbuwalo@gmail.com>
2024-08-05 14:56:19 +0100

    feat: done with auth

--- src/app/features/auth/sign-in/sign-in.component.ts
+++ src/app/features/auth/sign-in/sign-in.component.ts
@@ -1,14 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { ToggleBtnComponent } from '../../../shared/components/toggle-btn/toggle-btn.component';
 import { LayoutService } from '../../../core/layout/services/layout.service';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [ToggleBtnComponent],
+  imports: [ToggleBtnComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
 export class SignInComponent {
   layoutService = inject(LayoutService);
+  router = inject(Router);
+  form = new FormGroup({
+    email: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
+
+  onSignIn() {
+    if (this.form.invalid) {
+      alert('Please fill all fields');
+      return;
+    }
+
+    this.router.navigate(['/', 'vision', 'dashboard']);
+    this.form.reset();
+  }
 }
--- src/app/features/auth/sign-up/sign-up.component.ts
+++ src/app/features/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToggleBtnComponent } from '../../../shared/components/toggle-btn/toggle-btn.component';
+import { LayoutService } from '../../../core/layout/services/layout.service';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ToggleBtnComponent, RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
 export class SignUpComponent {
+  layoutService = inject(LayoutService);
+  router = inject(Router);
+  form = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
 
+  onSignIn() {
+    if (this.form.invalid) {
+      alert('Please fill all fields');
+      return;
+    }
+
+    this.router.navigate(['/', 'vision', 'dashboard']);
+    this.form.reset();
+  }
 }
