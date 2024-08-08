a0ad5e2d0a976e5365aad24895d45ed72bcf9d44
David Egbuwalo <lereegbuwalo@gmail.com>
2024-08-08 14:32:41 +0100

    refactor: some more style changes

--- src/app/features/dashboard/orders-overview/orders-overview.component.ts
+++ src/app/features/dashboard/orders-overview/orders-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orders-overview',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './orders-overview.component.html',
   styleUrl: './orders-overview.component.scss',
 })
-export class OrdersOverviewComponent implements OnInit {
+export class OrdersOverviewComponent {
   ordersOverview: { order: string; date: string }[] = [
     { order: '$2400 design changes', date: '22 DEC 7:20 PM' },
     { order: 'NEW order', date: '23 DEC 7:00 PM' },
@@ -16,10 +16,6 @@ export class OrdersOverviewComponent implements OnInit {
     { order: 'Unlocked packages for Development', date: '22 DEC 7:20 PM' },
     { order: 'New order', date: '22 DEC 7:20 PM' },
     { order: 'New order', date: '22 DEC 7:20 PM' },
-    // { order: 'New order', date: '22 DEC 7:20 PM' },
+    { order: 'New order', date: '22 DEC 7:20 PM' },
   ];
-
-  ngOnInit() {
-    // this.ordersOverview = [...this.ordersOverview, ...this.ordersOverview];
-  }
 }
